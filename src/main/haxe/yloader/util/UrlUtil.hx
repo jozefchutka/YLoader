f3c174c0b651b50afd392e01457d8467ac548bcd
Jozef Chutka <jozefchutka@gmail.com>
2018-08-07 17:57:11 +0200

    added nodejs version of UrlUtil and test

--- src/main/haxe/yloader/util/UrlUtil.hx
+++ src/main/haxe/yloader/util/UrlUtil.hx
@@ -1,23 +1,36 @@
 package yloader.util;
 
-import js.Browser;
 import yloader.valueObject.ParsedUrl;
 
 class UrlUtil
 {
 	public static function parse(url:String):ParsedUrl
 	{
-		var parser = Browser.document.createAnchorElement();
+		#if nodejs
+		var parser = js.node.Url.parse(url, true);
+		var auth = parser.auth;
+		var username = auth != null ? auth.split(":")[0] : null;
+		var password = auth != null ? auth.split(":")[1] : null;
+		return new ParsedUrl(parser.protocol, username, password, parser.host, parser.hostname,
+			parser.port, parser.pathname, parser.search, parser.hash);
+		#else
+		var document = js.Browser.document;
+		var parser = document.createAnchorElement();
 		parser.href = url;
 
 		// IE is missing slash in hostname if Anchor not added to document.
 		// https://stackoverflow.com/questions/956233/javascript-pathname-ie-quirk
 		// https://connect.microsoft.com/ie/feedback/details/817343
 		// https://connect.microsoft.com/IE/Feedback/Details/1002846
-		if(parser.pathname.substr(0, 1) != "/" && Browser.document.head != null)
-			Browser.document.head.appendChild(parser).parentElement.removeChild(parser);
+		if(parser.pathname.substr(0, 1) != "/" && document.head != null)
+			document.head.appendChild(parser).parentElement.removeChild(parser);
 
-		return new ParsedUrl(parser.protocol, parser.username, parser.password, parser.host, parser.hostname,
-			parser.port, parser.pathname, parser.search, parser.hash);
+		var port = parser.port == "" ? null : parser.port;
+		var username = parser.username == "" ? null : parser.username;
+		var password = parser.password == "" ? null : parser.password;
+		var hash = parser.hash == "" ? null : parser.hash;
+		return new ParsedUrl(parser.protocol, username, password, parser.host, parser.hostname,
+			port, parser.pathname, parser.search, hash);
+		#end
 	}
 }
---
+++ src/test/haxe/yloader/util/UrlUtilTest.hx
@@ -0,0 +1,60 @@
+package yloader.util;
+
+import massive.munit.Assert;
+
+class UrlUtilTest
+{
+	@BeforeClass
+	public function beforeClass():Void
+	{
+	}
+
+	@AfterClass
+	public function afterClass():Void
+	{
+	}
+
+	@Before
+	public function setup():Void
+	{
+	}
+
+	@After
+	public function tearDown():Void
+	{
+	}
+
+	@Test
+	public function customSimpleUrl_parse_matches():Void
+	{
+		var url = "https://github.com";
+		var result = UrlUtil.parse(url);
+
+		Assert.areEqual("https:", result.protocol);
+		Assert.areEqual(null, result.username);
+		Assert.areEqual(null, result.password);
+		Assert.areEqual("github.com", result.host);
+		Assert.areEqual("github.com", result.hostname);
+		Assert.areEqual(null, result.port);
+		Assert.areEqual("/", result.pathname);
+		Assert.areEqual("", result.search);
+		Assert.areEqual(null, result.hash);
+	}
+
+	@Test
+	public function customComplexUrl_parse_matches():Void
+	{
+		var url = "https://jozefchutka:pwd@www.github.com:80/jozefchutka/YLoader?a=b&c=d#myHash";
+		var result = UrlUtil.parse(url);
+
+		Assert.areEqual("https:", result.protocol);
+		Assert.areEqual("jozefchutka", result.username);
+		Assert.areEqual("pwd", result.password);
+		Assert.areEqual("www.github.com:80", result.host);
+		Assert.areEqual("www.github.com", result.hostname);
+		Assert.areEqual("80", result.port);
+		Assert.areEqual("/jozefchutka/YLoader", result.pathname);
+		Assert.areEqual("?a=b&c=d", result.search);
+		Assert.areEqual("#myHash", result.hash);
+	}
+}
